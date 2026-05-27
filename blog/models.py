b3f4b48093069b9264230b5ac890c9abaaf443ea
lakpalama7 <sherpasvcet@gmail.com>
2026-05-27 22:15:18 +0300

    table created in database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='static/images', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
