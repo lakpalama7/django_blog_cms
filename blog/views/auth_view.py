4d506e72f48c84aa85851668acb7e8e8cedc3156
lakpalama7 <sherpasvcet@gmail.com>
2026-05-28 12:53:12 +0300

    create blog and user registration complete

--- blog/views/auth_view.py
+++ blog/views/auth_view.py
@@ -1,9 +1,24 @@
 
 
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def register(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.create(
+            email = email,
+            username = username,
+        )
+        user.set_password(password) #hashing password
+        user.save()
+        return redirect('login')
+
+        
     return render(request, 'auth/register.html')
 
 def login(request):
